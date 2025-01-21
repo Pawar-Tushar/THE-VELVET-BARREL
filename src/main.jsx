15aa9c72562d7220cf316a265c40f0324bc06165
Pawar-Tushar <157030970+Pawar-Tushar@users.noreply.github.com>
2025-01-21 23:07:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./App.jsx";
-import "./app.css";
+import "app.css";
 import Store from "./redux/Store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
